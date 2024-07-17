25da298e8af1e52ee17a2705e740a5b291128d81
Razzakh <mrazzakh123@gmail.com>
2024-07-17 11:47:18 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,15 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [render,setrender]=useState(false);
+const handlebtn=()=>{
+  setrender(true);
+}
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      <button onClick={handlebtn} id="click">click</button>
+      {render && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> }
     </div>
   );
 }
